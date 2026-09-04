63c492996d009d0653af5ba0262c36605843902f
AgentMega <39720024+AgentMega@users.noreply.github.com>
2026-09-04 17:20:25 +0700

    dd

--- Assets/_SCRIPTS/WallObject.cs
+++ Assets/_SCRIPTS/WallObject.cs
@@ -7,16 +7,20 @@ namespace Destrial
     public class WallObject : CellObject
     {
         public Tile ObstacleTile;
+        public Tile DestroyTile1;
+        public Tile DestroyTile2;
         public int MaxHealth = 3;
 
         private int _healthPoint;
         private Tile _originalTile;
 
+        private Vector2Int myPos;
+
         public override void Init(Vector2Int cell)
         {
             base.Init(cell);
             _healthPoint = MaxHealth;
-
+            myPos = cell;
             _originalTile = GameManager.Instance.BoardManager.GetCellTile(cell);
             GameManager.Instance.BoardManager.SetCellTile(cell, ObstacleTile);
         }
@@ -24,6 +28,14 @@ namespace Destrial
         public override bool PlayerWantsToEnter()
         {
             _healthPoint -= 1;
+            if (_healthPoint == 2)
+            {
+                GameManager.Instance.BoardManager.SetCellTile(myPos, DestroyTile1);
+            }
+            else if (_healthPoint == 1)
+            {
+                GameManager.Instance.BoardManager.SetCellTile(myPos, DestroyTile2);
+            }
 
             if (_healthPoint > 0)
             {
